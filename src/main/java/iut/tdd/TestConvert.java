613e7e2bb2837271c3a578899ad224bd74194f21
Hexafile <thomas.plouchart@hotmail.fr>
2015-03-30 16:57:50 +0200

    Cmt 4

--- src/main/java/iut/tdd/TestConvert.java
+++ src/main/java/iut/tdd/TestConvert.java
@@ -10,14 +10,6 @@ public class TestConvert {
 		Assert.assertEquals("zéro", Convert.num2text("0"));
 	}
 	@Test
-	public void test_num2text_un () {
-		Assert.assertEquals("un", Convert.num2text("1"));
-	}
-	@Test
-	public void test_num2text_deux () {
-		Assert.assertEquals("deux", Convert.num2text("2"));
-	}
-	@Test
 	public void test_num2text_neuf () {
 		Assert.assertEquals("neuf", Convert.num2text("9"));
 	}
@@ -26,11 +18,23 @@ public class TestConvert {
 		Assert.assertEquals("seize", Convert.num2text("16"));
 	}
 	@Test
-	public void test_num2text_dixsept () {
-		Assert.assertEquals("dix-sept", Convert.num2text("17"));
+	public void test_num2text_vingttrois () {
+		Assert.assertEquals("vingt-trois", Convert.num2text("23"));
 	}
 	@Test
-	public void test_num2text_dixneuf () {
-		Assert.assertEquals("dix-neuf", Convert.num2text("19"));
+	public void test_num2text_soixanteonze () {
+		Assert.assertEquals("soixante-et-onze", Convert.num2text("71"));
+	}
+	@Test
+	public void test_num2text_soixantequinze () {
+		Assert.assertEquals("soixante-quinze", Convert.num2text("75"));
+	}
+	@Test
+	public void test_getDizaine_30 () {
+		Assert.assertEquals("30", Convert.getDizaine(33));
+	}
+	@Test
+	public void test_getDizaine_40 () {
+		Assert.assertEquals("40", Convert.getDizaine(45));
 	}
 }
